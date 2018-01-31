b6867784f38b05b821f127d3b04b507feb1c72b3
munHunger <marcusmunger@hotmail.com>
2018-01-31 19:59:28 +0100

    Added runtime settings to db

--- oven/src/main/java/se/munhunger/oven/dao/DatabaseDAO.java
+++ oven/src/main/java/se/munhunger/oven/dao/DatabaseDAO.java
@@ -5,6 +5,9 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Map;
 
 public class DatabaseDAO {
     protected static SessionFactory sessionFactory;
@@ -16,11 +19,17 @@ public class DatabaseDAO {
 
     private static void init()
     {
+        Map<String, String> env = System.getenv();
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure()
                 .build();
-        MetadataSources mds = new MetadataSources(registry);
-        Metadata md = mds.buildMetadata();
-        sessionFactory = md.buildSessionFactory();
+        Configuration config = new Configuration();
+        if(env.containsKey("DB_URL"))
+            config.setProperty("hibernate.connection.url", "jdbc:" + env.get("DB_URL"));
+        if(env.containsKey("DB_USER"))
+            config.setProperty("hibernate.connection.username", env.get("DB_USER"));
+        if(env.containsKey("DB_PASS"))
+            config.setProperty("hibernate.connection.password", env.get("DB_PASS"));
+        sessionFactory = config.buildSessionFactory(registry);
         Runtime.getRuntime().addShutdownHook(new Thread(() -> StandardServiceRegistryBuilder.destroy(registry)));
     }
 
